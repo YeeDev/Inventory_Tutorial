7c939932b1abb394044d39c2f87c3ff470fdf29c
Yee <yee.gamedev@gmail.com>
2021-08-14 14:05:00 -0500

    Adding Items to Inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField] Transform inventorySpace = null;
+
+    public void AddItem(GameObject itemToAdd)
+    {
+        Instantiate(itemToAdd, inventorySpace);
+    }
+}
--- Assets/Scripts/Picker.cs
+++ Assets/Scripts/Picker.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Picker : MonoBehaviour
 {
+    Inventory inventory;
+
+    private void Awake()
+    {
+        inventory = FindObjectOfType<Inventory>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Always use CompareTag, it is way faster.
         if (collision.CompareTag("Item"))
         {
+            inventory.AddItem(collision.GetComponent<Itemizer>().GetItemToAdd);
             Destroy(collision.gameObject);
         }
     }
