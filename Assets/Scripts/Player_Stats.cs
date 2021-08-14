30adbdbbff6573bca9b4d60e19eea47a832299b2
Yee <yee.gamedev@gmail.com>
2021-08-14 13:24:20 -0500

    Max Stats and Clamps

--- Assets/Scripts/Player_Stats.cs
+++ Assets/Scripts/Player_Stats.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class Player_Stats : MonoBehaviour
 {
-    [SerializeField] float currentHp = 0;
-    [SerializeField] float currentMp = 0;
+    [Header("Health Points")]
+    [Range(1, 10)][SerializeField] float currentHp = 0;
+    [Range(1, 10)] [SerializeField] float maxHP = 3;
+    [Header("Magic Points")]
+    [Range(1, 10)] [SerializeField] float currentMp = 0;
+    [Range(1, 10)] [SerializeField] float maxMP = 10;
 
     UI_Controller uI;
 
     private void Awake()
     {
         uI = FindObjectOfType<UI_Controller>();
+        ClampCurrentValues();
+    }
+
+    //Prevents having unwanted values, like having a current HP larger than max HP.
+    //You could create a Custom Inspector to save you this lines of code!
+    private void ClampCurrentValues()
+    {
+        currentHp = Mathf.Clamp(currentHp, 1, maxHP);
+        currentMp = Mathf.Clamp(currentMp, 1, maxMP);
+    }
+
+    //This is called in Start instead of Awake to avoid racing issues.
+    private void Start()
+    {
+        InitializeUI();
     }
 
     //Set the size of the stats bars based on "current" variables.
-    private void Start()
+    private void InitializeUI()
     {
+        uI.SetMaxSize(maxHP, maxMP);
         uI.SetBarSize(true, currentHp);
         uI.SetBarSize(false, currentMp);
     }
--- Assets/Scripts/UI_Controller.cs
+++ Assets/Scripts/UI_Controller.cs
@@ -8,25 +8,39 @@ public class UI_Controller : MonoBehaviour
     //You could always code getting this value, but I find it easier this way.
     [Header("Health Power")]
     [Tooltip("Based on the Heart Bar single heart width value")]
-    [SerializeField] float hPSize = 0;
+    [SerializeField] float hPSizePerPoint = 0;
     [SerializeField] RectTransform heartBar = null;
 
     //In the case of the mana size per point, it should be coded based on the mana maximum value.
     //But for simplicity purposes I serialized it.
     [Header("Magic Power")]
     [Tooltip("Based on the Mana Bar max width value divided by the max number of mana points")]
-    [SerializeField] float mPSize = 0;
+    [SerializeField] float mPSizePerPoint = 0;
     [SerializeField] RectTransform manaBar = null;
+
+    float maxHPSize;
+    float maxMPSize;
+
+    //Sets the maximum width value.
+    //Called from Player_Stats.
+    public void SetMaxSize(float maxHP, float maxMP)
+    {
+        maxHPSize = hPSizePerPoint * maxHP;
+        print(maxHPSize);
+        maxMPSize = mPSizePerPoint * maxMP;
+        print(maxMPSize);
+    }
 
     //Adjust the size of any of the bars based on the passed bool.
     //Called from Player_Stats and ItemAbilities scripts.
     public void SetBarSize(bool adjustHealth, float amount)
     {
-        float statToModify = adjustHealth ? hPSize * amount : mPSize * amount;
+        float statToModify = adjustHealth ? hPSizePerPoint * amount : mPSizePerPoint * amount;
+        float maxStatToUse = adjustHealth ? maxHPSize : maxMPSize; 
         RectTransform barToModify = adjustHealth ? heartBar : manaBar;
 
         Vector2 newSize = barToModify.sizeDelta;
-        newSize.x += statToModify;
+        newSize.x += Mathf.Clamp(statToModify, 0, maxStatToUse);
         barToModify.sizeDelta = newSize;
     }
 }
